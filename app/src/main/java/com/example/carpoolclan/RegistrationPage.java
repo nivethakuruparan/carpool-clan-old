8f271a7ceeb9b98e3e59b0b1aec7f3f7bf63a2c6
jinalkast <jinalkast@gmail.com>
2023-03-31 15:12:43 -0400

    configured gradle for firebase

--- app/src/main/java/com/example/carpoolclan/RegistrationPage.java
+++ app/src/main/java/com/example/carpoolclan/RegistrationPage.java
@@ -3,12 +3,26 @@ package com.example.carpoolclan;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.*;
 
 public class RegistrationPage extends AppCompatActivity {
+
+    EditText signupName, signupEmail, signupDOB, signupPassword;
+    TextView loginRedirectText;
+    Button signupButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration_page);
+
+        name = findViewById(R.id.signup_name);
+        email = findViewById(R.id.signup_email);
+        dob = findViewById(R.id.signup_dob);
+        password = findViewById(R.id.signup_password);
+    }
+
+    public void displayUserData() {
+
     }
 }
