b7c4ebeab3cd7c06d1c2d1ac7d8799ce8bd6383e
Nivetha Kuruparan <nivetha.kuruparan@gmail.com>
2023-04-03 16:47:18 -0400

    implemented google maps

---
+++ app/src/main/java/com/example/carpoolclan/GoogleMapsInterface.java
@@ -0,0 +1,42 @@
+package com.example.carpoolclan;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+import android.widget.FrameLayout;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class GoogleMapsInterface extends FragmentActivity implements OnMapReadyCallback {
+
+    GoogleMap gMap;
+    FrameLayout map;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_google_maps_interface);
+
+        map = findViewById(R.id.map);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        this.gMap = googleMap;
+
+        LatLng mapIndia = new LatLng(20.5837, 78.9629);
+        this.gMap.addMarker(new MarkerOptions().position(mapIndia).title("Marker in India"));
+        this.gMap.moveCamera(CameraUpdateFactory.newLatLng(mapIndia));
+    }
+}
