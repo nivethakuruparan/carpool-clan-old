118d2f445268b744606970dcc0df5050234f9a2f
AadilRn <aadilrehan02@gmail.com>
2023-04-06 22:30:12 -0400

    Added stars and buttons to RatingPage

--- app/src/main/java/com/example/carpoolclan/RatingPage.java
+++ app/src/main/java/com/example/carpoolclan/RatingPage.java
@@ -1,4 +1,13 @@
 package com.example.carpoolclan;
 
-public class RatingPage {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RatingPage extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rating_page);
+    }
 }
