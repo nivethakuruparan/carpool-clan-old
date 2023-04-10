ef7aeab1828d5364c31f25d283a3b4935d969e17
Nivetha Kuruparan <nivetha.kuruparan@gmail.com>
2023-04-10 03:09:19 -0400

    merged aadil's home page and edited functionality to include more pages

--- app/src/main/java/com/example/carpoolclan/TripDurationController.java
+++ app/src/main/java/com/example/carpoolclan/TripDurationController.java
@@ -4,14 +4,37 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TripDurationController {
+    SessionController session;
+    private boolean tripStatus;
+    private String customerType;
+    private String polyLine;
+
     // the list of customerID's that is displayed to an individual user
     // every user will have a different customerIDs list
     private static List<String> customerIDs;
 
     public TripDurationController() {
+        session = new SessionController();
+
+        tripStatus = SessionController.getTripStatus();
+        customerType = SessionController.getCustomerType();
+        polyLine = SessionController.getPolyLine();
+
         customerIDs = new ArrayList<>();
 
         addRatingCard();
+    }
+
+    public boolean getTripStatus() {
+        return tripStatus;
+    }
+
+    public String getCustomerType() {
+        return customerType;
+    }
+
+    public String getPolyLine() {
+        return polyLine;
     }
 
     public String getCustomerID(int position) { return customerIDs.get(position); }
@@ -28,6 +51,8 @@ public class TripDurationController {
     public void addRatingCard() {
         // this is all the DB stuff; including all passengers except for the user
         // iterate through DB and add to the list here
+        // make sure to not add the current user ID
+
         String customerID_A = "bobby123.john@gmail.com";
         String customerID_B = "john.bob@gmail.com";
         String customerID_C = "sean.john@gmail.com";
